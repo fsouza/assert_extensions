265ba4cede8b7714b9a3c90f0218c9d3b177541c
Francisco Souza <francisco@franciscosouza.net>
2011-09-01 21:38:28 -0300

    Added another test, still failing

--- test_assert_extension.c
+++ test_assert_extension.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include "assert_extensions.h"
 
 int test_count = 0;
 
@@ -25,8 +26,17 @@ void test_assert_equal_for_integer_values() {
     extern int test_count;
     test_count++;
 
-    printf("assert_equal(1, 1); should run nicely...");
+    printf("assert_equal(1, 1); should run nicely... ");
     assert_equal(1, 1);
+    printf("ok\n");
+}
+
+void test_assert_equal_should_quit_program_for_non_equal_int_values() {
+    extern int test_count;
+    test_count++;
+
+    printf("assert_equal(1, 2); should exit with status code 1... ");
+    assert_equal(1, 2);
     printf("ok\n");
 }
 
@@ -34,6 +44,7 @@ int main (int argc, char const* argv[]) {
     test_check_equal_should_return_zero_for_non_equal_int_values();
     test_check_equal_should_return_non_zero_for_equal_int_values();
     test_assert_equal_for_integer_values();
+    test_assert_equal_should_quit_program_for_non_equal_int_values();
 
     extern int test_count;
     printf("\n\nRan %d tests.\n\n", test_count);
